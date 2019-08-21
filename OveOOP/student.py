aa0f413844e1e600565bd03ab52fcd0c66d18234
Jorunn mac <jorunn@Jorunns-MacBook-Pro.local>
2019-08-21 23:28:36 +0200

    student

---
+++ OveOOP/student.py
@@ -0,0 +1,30 @@
+class student:
+    def __init__(self, name, aer):
+        self.name = name
+        self.aer = aer
+        def nya(self):
+            self.aer += 1
+            pass
+
+class smarting(student):
+    def __init__(self, name, aer):
+        self.name = name
+        self.aer = aer
+        self.smart = True
+
+class dumming(student):
+    def __init__(self, name, aer):
+        self.name = name
+        self.aer = aer
+        self.smart = False
+
+p = student("Per",4)
+print(p.name)
+j = smarting("Jørgen", 7)
+print(j.name)
+print(j.smart)
+d = dumming("dumgutt", 0)
+print(d.name)
+print(d.smart)
+p.nya()
+print(p.aer)
